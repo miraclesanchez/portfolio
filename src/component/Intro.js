ea407087f0d308fa23f00da166dac22aafa2d371
Miracle Sanchez <miracle.sanchez19@stjohns.edu>
2024-01-10 18:49:21 -0500

    Fixing view for small screen

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -4,7 +4,7 @@ import resume from "../Miracle-Sanchez-Resume-Web.pdf"
 export default function Bio (){
     return(
         <div className="intro" id="home">
-            <div className="about-me">
+            <div className="intro">
                 <h3 className="name-title">Hi, I'm <span>Miracle Sanchez</span>!</h3>
                 <h3 className="role">Software Developer</h3>
                 <p>Good to see you here!!<br/>I hold a <span>B.A. in Computer Science</span> and <br/>am looking for new opportunities.</p>
