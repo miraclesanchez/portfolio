c80eb4c35a75922422bcfa4287f3cf51955e3056
Miracle Sanchez <miracle.sanchez19@stjohns.edu>
2024-01-23 15:48:53 -0500

    Fixed projects display and display them using react props

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import Skills from "./component/Skills";
 import Footer from "./component/Footers";
 import Projects from "./component/Projects";
 import About from "./component/About";
+import data from "./data";
 
 export default function app(){
+  const projects = data.map(project => {
+    return(
+      <Projects
+        key={project}
+        project={project}
+        title={"Projects"}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar/>
       <Bio/>
       <Skills/>
-      <Projects/>
+      <h1 className="projects-title">
+        Projects
+      </h1>
+      <section id="project-section"> 
+        {projects}
+      </section>
       <About/>
       <Footer/>
     </div>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,25 +1,74 @@
 import React from "react";
-// import {useState} from "react"
-import {Link} from "react-scroll"
-import menu from "../images/menu-logo.png"
-import logo from "../images/my-logo.png"
-import close from "../images/close.png"
+import { useState } from "react";
+import { Link } from "react-scroll";
+import menu from "../images/menu-logo.png";
+import logo from "../images/my-logo.png";
+// import close from "../images/close.png";
 
-export default function Navbar () {
-    return (
-        <div className="navbar">
-            <img src={logo} alt="my-logo" className="my-logo"/> 
-            <nav className="nav">
-                <ul className="menu-items">
-                    <li><Link to="home" spy={true} smooth={true} offset={-100} duration= {500}>Home</Link></li>
-                    <li><Link to="skills" spy={true} smooth={true} offset={-100} duration={500}>Skills</Link></li>
-                    <li><Link to="projects" spy={true} smooth={true} offset={-100} duration={500}>Projects</Link></li>
-                    <li><Link to="about" spy={true} smooth={true} offset={-100} duration={500}>About</Link></li>
-                    {/* <img src={close} alt="close-logo" className="close-logo"/> */}
-                </ul>
-                
-            </nav>
-            <img src={menu} alt="menu-logo" className="menu-logo"/>
-        </div>
-    )
+export default function Navbar() {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const handleClick = (link) => {
+    setActiveLink(link);
+
+  };
+  return (
+    <div className="navbar">
+      <img src={logo} alt="my-logo" className="my-logo" />
+      <nav className="nav">
+        <ul className="menu-items">
+          <li className={activeLink === "home" ? "active-link" : ""}>
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={() => handleClick("home")}
+            >
+              Home
+            </Link>
+          </li>
+          <li className={activeLink === "skills" ? "active-link" : ""}>
+            <Link
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={() => handleClick("skills")}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className={activeLink === "projects" ? "active-link" : ""}>
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={() => handleClick("projects")}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className={activeLink === "about" ? "active-link" : ""}>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={() => handleClick("about")}
+            >
+              About
+            </Link>
+          </li>
+          {/* <img src={close} alt="close-logo" className="close-logo"/> */}
+        </ul>
+      </nav>
+      <img src={menu} alt="menu-logo" className="menu-logo" />
+    </div>
+  );
 }
