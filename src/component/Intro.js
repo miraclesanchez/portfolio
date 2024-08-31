f312ffa471645ed32fab3261d344c7082a4464c8
Miracle Sanchez <miracle.sanchez19@stjohns.edu>
2024-08-31 12:39:53 -0400

    Revised intro to include MS

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -7,7 +7,7 @@ export default function Bio (){
             <div className="intro">
                 <h3 className="name-title">Hi, I'm <span>Miracle Sanchez</span>!</h3>
                 <h3 className="role">Software Developer</h3>
-                <p>Good to see you here!!<br/>I hold a <span>B.S. in Computer Science</span> and <br/>am looking for new opportunities.</p>
+                <p>Good to see you here!!<br/>I hold a <span>B.S. in Computer Science</span> and am currently pursuing an <span>M.S. in Computer Science</span><br/></p>
                 <a href={resume} download="Miracle_Sanchez_Resume.pdf"><button className="resume-button">Download Resume</button></a>
             </div>
         </div>
